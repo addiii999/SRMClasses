062f0fcc7af9705447ae6e10e0f0f969f6c18214
addiii999 <adityapandeypandey019@gmail.com>
2026-03-21 04:37:20 +0530

    Update frontend API URL to point to Hostinger production backend

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.MODE === 'development' ? '/api' : 'https://api.srmclasses.in/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
