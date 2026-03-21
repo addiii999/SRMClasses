4254270d87aa067e1b9a3b20b401bd8d4bc16495
addiii999 <adityapandeypandey019@gmail.com>
2026-03-22 01:55:44 +0530

    Fix OTP sending: use Gmail service explicitly

--- server/controllers/adminAuthController.js
+++ server/controllers/adminAuthController.js
@@ -49,8 +49,7 @@ const adminForgotPassword = async (req, res) => {
     await admin.save({ validateBeforeSave: false });
 
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      service: 'gmail',
       auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
     });
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -72,8 +72,7 @@ const forgotPassword = async (req, res) => {
     await user.save({ validateBeforeSave: false });
 
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      service: 'gmail',
       auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
     });
 
