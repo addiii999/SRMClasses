c29412bdfa11eeef28cece865a4368f2a83148df
addiii999 <adityapandeypandey019@gmail.com>
2026-03-24 00:55:53 +0530

    Fix Google Maps missing pin by regenerating embed URL

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -171,7 +171,7 @@ export default function Contact() {
         <div className="h-[450px] bg-gray-100">
           <iframe
             title="SRM Classes - Ranchi Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.8454228491843!2d85.26510447545195!3d23.393814278913955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4de05a37f8591%3A0xd9b09379e246fa39!2sSrm%20Classes!5e0!3m2!1sen!2sin!4v1742747814440!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.8452668403957!2d85.26510447478121!3d23.393814302373514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4de05a37f8591%3A0xd9b09379e246fa39!2sSrm%20Classes!5e0!3m2!1sen!2sin!4v1774293801590!5m2!1sen!2sin"
             width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"
           />
         </div>
